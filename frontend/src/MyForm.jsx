a68d050c8f10d62395877912a9480a27a8d7fe38
Umutcan Çetin <66206623+NeuralG@users.noreply.github.com>
2025-09-30 22:24:15 +0300

    Made it more responsive

--- frontend/src/MyForm.jsx
+++ frontend/src/MyForm.jsx
@@ -2,20 +2,18 @@ import { FiSettings } from "react-icons/fi"
 
 function MyForm({ query, setQuery, setIsOptionsHidden, handleSubmit }) {
 	return (
-		<div className="form-container">
+		<form className="form-container" onSubmit={handleSubmit}>
 			<button onClick={() => setIsOptionsHidden(false)}>
 				<FiSettings style={{ fontSize: "20px" }} />
 			</button>
-			<form onSubmit={handleSubmit}>
-				<input
-					type="text"
-					placeholder="Enter drone question..."
-					value={query}
-					onChange={(e) => setQuery(e.target.value)}
-				/>
-				<button type="submit">Submit!</button>
-			</form>
-		</div>
+			<input
+				type="text"
+				placeholder="Enter drone question..."
+				value={query}
+				onChange={(e) => setQuery(e.target.value)}
+			/>
+			<button type="submit">Submit!</button>
+		</form>
 	)
 }
 
